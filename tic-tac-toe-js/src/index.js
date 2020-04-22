826cbae10730e4f37b9771fb8da0fea106f97622
Shamus Clifford <scliff108@gmail.com>
2020-04-22 17:45:55 -0400

    Update board creation
    Use map instead of hardcoading board

--- tic-tac-toe-js/src/index.js
+++ tic-tac-toe-js/src/index.js
@@ -17,6 +17,7 @@ class Board extends React.Component {
   renderSquare(i) {
       return (
         <Square
+          key={i}
           value={this.props.squares[i]}
           onClick={() => this.props.onClick(i)}
         />
@@ -24,26 +25,15 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-    </div>
-  );
-}
+    let rows = [0,1,2].map(row => {
+      let squares = [0,1,2].map(col => {
+        return this.renderSquare(row * 3 + col);
+      });
+      return <div className="board-row" key={row}>{squares}</div>;
+    });
+
+    return <div>{rows}</div>;
+  }
 }
 
 class Game extends React.Component {
