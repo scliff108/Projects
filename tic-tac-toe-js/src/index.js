cb44f29b9a9b80dd3d66938144b3a050d42f1171
Shamus Clifford <scliff108@gmail.com>
2020-04-22 17:48:53 -0400

    Change ordered list to unordered

--- tic-tac-toe-js/src/index.js
+++ tic-tac-toe-js/src/index.js
@@ -116,7 +116,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
+          <ul>{moves}</ul>
         </div>
       </div>
     );
