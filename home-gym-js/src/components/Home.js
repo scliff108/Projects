fd73f6cad92d6c969156074894237f1f77ce188f
Shamus Clifford <scliff108@gmail.com>
2020-04-24 15:51:19 -0400

    Add very basic home page

--- home-gym-js/src/components/Home.js
+++ home-gym-js/src/components/Home.js
@@ -3,8 +3,30 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to at Home Workout center.</p>
+      <h2>Materials</h2>
+      <ul>
+        <li>2 Water Jugs</li>
+        <li>T-Shirt (preferably long sleeve)</li>
+        <li>Hand Towel</li>
+        <li>
+          2 Small Weight
+          <ul>
+            <li>Water Bottles</li>
+            <li>Gatorade Bottles</li>
+            <li>Canned Goods</li>
+          </ul>
+        </li>
+      </ul>
+
+      <h2>Making the Water Jug Barbell</h2>
+      <p>Instructions for making the water jug barbell</p>
+
+      <h2>Free Apps</h2>
+      <ul>
+        <li><a href="https://www.nike.com/ntc-app" target="_blank" rel="noopener noreferrer">Nike Training Club</a> (free until further notice)</li>
+        <li><a href="https://www.onepeloton.com/app" target="_blank" rel="noopener noreferrer">Peloton</a> (90-day free trial)</li>
+        <li><a href="https://dailyburn.com/landing" target="_blank" rel="noopener noreferrer">Daily Burn</a> (30-day free trial)</li>
+      </ul>
     </div>
   );
 }
