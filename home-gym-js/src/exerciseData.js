fb2291291d30bd333dc25b52adb365f5fb8a8aad
Shamus Clifford <scliff108@gmail.com>
2020-04-24 13:21:42 -0400

    Add Exercise names and categories

--- home-gym-js/src/exerciseData.js
+++ home-gym-js/src/exerciseData.js
@@ -4,6 +4,7 @@ export default [
     {
         "exercise": "Push Up",
         "description": "The push-up is a popular bodyweight exercise that is commonly used in military and tactical physical fitness tests. It’s a classic movement to build upper-body muscle and strength, emphasizing the chest, triceps, and shoulders, but also working the upper back and core.",
+        "category": "Chest",
         "bodyparts": [
             "Chest",
             "Triceps",
@@ -17,5 +18,580 @@ export default [
             "end": pushupImg
         },
         "reps": [10, 20, 30]
-    }
+    },
+    {
+        "exercise": "Squat",
+        "description": "The squat is a fundamental exercise that strengthens the quads, glutes, and hamstrings. It can be performed as part of a dynamic warm-up, for time or reps during a workout, or for hip and spine mobility any time.",
+        "category": "Legs",
+        "bodyparts": [
+            "Quadriceps",
+            "Hamstrings"
+        ],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": [10, 20, 30]
+    },
+    {
+        "exercise": "Sit Up",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Leg Lift",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Bicycle Crunch",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Plank",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Side Plank",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Knee Bends",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+        "exercise": "Standing Broom Twists",
+        "description": "",
+        "category": "Core",
+        "bodyparts": [],
+        "equipment": [],
+        "images": {
+            "thumbnail": pushupImg,
+            "start": pushupImg,
+            "end": pushupImg
+        },
+        "reps": []
+    },
+    {
+    "exercise": "Russian Twists",
+    "description": "",
+    "category": "Core",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Oblique Heel Taps",
+    "description": "",
+    "category": "Core",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Lunge",
+    "description": "",
+    "category": "Legs",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Calf Raise",
+    "description": "",
+    "category": "Legs",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Wall Taps",
+    "description": "",
+    "category": "Legs",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Single Leg Push Off",
+    "description": "",
+    "category": "Legs",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Burpee",
+    "description": "",
+    "category": "Legs",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Pull Up",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Bent Over Rows",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Chair Rows",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Back Fly",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "T-Shirt Lat Pull Down",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Towel Pull Up",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Jefferson Curl",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Dead Lift",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Superman",
+    "description": "",
+    "category": "Back",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Close Grip Push Up",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Wide Grip Push Up",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Incline Push Up",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Decline Push Up",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Door Squeeze Fly",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Chest Squeeze",
+    "description": "",
+    "category": "Chest",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Shoulder Press",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Arnold Press",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Front-Shoulder Raise",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Lateral Raise",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Shrugs",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Upright Row",
+    "description": "",
+    "category": "Shoulders",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "T-Shirt Barbell Curl",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "T-Shirt Dumbell Curl",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Table Curl",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Chair Dips",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Easy Chair Dips",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Table Skull Crusher",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Bottle Skull Crusher",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
+    {
+    "exercise": "Tricep Overhead Extensions",
+    "description": "",
+    "category": "Arms",
+    "bodyparts": [],
+    "equipment": [],
+    "images": {
+    "thumbnail": pushupImg,
+    "start": pushupImg,
+    "end": pushupImg
+    },
+    "reps": [],
+    },
 ];
